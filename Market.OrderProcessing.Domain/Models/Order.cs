d63bb06d01fb4dec5f0a8bf00854b69432fac263
Ogabek <hamzaqulovogabek@gmail.com>
2024-08-18 15:04:38 +0500

    Add project files.

---
+++ Market.OrderProcessing.Domain/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using Market.Warehouse.Domain.Models;
+
+namespace Market.OrderProcessing.Domain.Models;
+
+public class Order : BaseEntity<int>
+{
+    public int UserId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalPrice { get; set; }
+
+}
